8701f98a28abd97e59191c343aed50eccd72426f
danyakovalugrasu <danyakovalugrasu@gmail.com>
2024-02-18 14:32:30 +0500

    Getting to know Laravel
    Creating routes that use methods to output information

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InformationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/info/server', [InformationController::class, 'serverInfo']);
+Route::get('/info/client', [InformationController::class, 'clientInfo']);
+Route::get('/info/database', [InformationController::class, 'databaseInfo']);
