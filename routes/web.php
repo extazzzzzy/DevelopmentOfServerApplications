8f66b5332c57f689b2fbdc386110508fdbb8a8fd
danyakovalugrasu <danyakovalugrasu@gmail.com>
2024-03-21 12:44:14 +0500

    creating empty routes

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => '/api/auth'], function () {
+
+    Route::post('/login', [RENAME::class, '']);
+    Route::post('/register', [RENAME::class, '']);
+    Route::get('/me', [RENAME::class, '']);
+    Route::post('/out', [RENAME::class, '']);
+    Route::get('/tokens', [RENAME::class, '']);
+    Route::post('/out_all', [RENAME::class, '']);
+
 });
 
