d6a908b00bf9b65706b6e5249be9184d3aac0b58
danyakovalugrasu <danyakovalugrasu@gmail.com>
2024-05-28 19:33:32 +0500

    ю

--- database/migrations/2024_05_20_202320_create_roles_table.php
+++ database/migrations/2024_05_20_202320_create_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,7 +17,7 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->string('description')->nullable();
             $table->string('cipher')->unique();
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrent();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->nullable();
             $table->integer('created_by');
             $table->softDeletes()->nullable(); // deleted_at
